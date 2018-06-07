cdffbf3080e85e717d83a05cfc5cdf1d80da49d9
LeeVaughn <dlvprogramming@gmail.com>
2018-06-07 12:58:40 -0700

    Added function to check if data file exists and create it if it doesn't.

--- scraper.js
+++ scraper.js
@@ -1,2 +1,14 @@
+const fs = require('fs');
 const scrapeIt = require("scrape-it");
 const Json2csvParser = require('json2csv').Parser;
+
+// checks if directory exists, and creates it if it doesn't
+function checkDirSync(dir) {
+    try {
+        fs.statSync(dir);
+    } catch(err) {
+        fs.mkdirSync(dir);
+    }
+}
+
+checkDirSync("data");
