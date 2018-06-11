67e49955e35f6ec605324a34fe47325db326f57d
LeeVaughn <dlvprogramming@gmail.com>
2018-06-11 16:59:17 -0700

    added function to return individual shirt urls

--- scraper.js
+++ scraper.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const scrapeIt = require("scrape-it");
 const Json2csvParser = require('json2csv').Parser;
+let shirtsRemaining = 0;
 
 // checks if directory exists, and creates it if it doesn't
 function checkDirSync(dir) {
@@ -23,8 +24,43 @@ function errorLog(error) {
     }
 
     fs.appendFile("scraper-error.log", `${errorDate}: ${error} \n`, (error) => {
-        if (error) throw (error);
+        if (error) {
+            throw (error);
+        }
     });
 }
 
+// scrapes site and creates links for individual shirts
+function scrapeShirtLinks () {
+    let shirtLinks = [];
+
+    // gets url from each list item
+    // adds urls to array
+    // calls scrapeShirt function using each object in the array as an argument
+    scrapeIt("http://shirts4mike.com/shirts.php", {
+        shirts: {
+            listItem: ".products li a",
+            data: {
+                url: {
+                    attr: "href"
+                }
+            }
+        }
+    }, (error, data) => {
+        if (error) {
+            errorLog(error);
+        } else {
+            shirtsRemaining = data.data.shirts.length;
+
+            for (let i =0; i < shirtsRemaining; i++) {
+                shirtLinks[i] = `http://shirts4mike.com/${data.data.shirts[i].url}`;
+                scrapeShirt(shirtLinks[i]);
+            }
+        }
+    });
+}
+
+// run on start
+console.log("scraper.js is now running.")
 checkDirSync("data");
+scrapeShirtLinks();
